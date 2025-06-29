c7db2e6ad83953e80c5f781b41b56dfde64a14f0
Abhishek kumar <abhi743525@gmail.com>
2025-06-29 06:55:10 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://pfm-backend-0qd8.onrender.com/";
+  import.meta.env.VITE_API_BASE_URL || "https://pfm-backend-0qd8.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
